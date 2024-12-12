979d173f7eb4a8f1cf2a4a0c976a185fd5d3e586
yousefosm25 <yousefmouse1@gmail.com>
2024-12-12 19:35:58 +0200

    update

--- Untitled-1.py
+++ Untitled-1.py
@@ -3,8 +3,8 @@ age = 38
 name = "Muhammed Essa"
 print (name)
 print (age)
-
+# variables can be changed
 num = 10 
 dgree = 55.2
+myname = 'yousef\'s havebook' #to ignore ' if i want to used it 
 
-print("hello world")
